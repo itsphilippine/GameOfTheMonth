3dc35eceb2a78823db684f179648354209bc115f
itsphilippine <pgadeau@artfx.fr>
2021-02-15 21:32:52 +0100

    FullGame

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+    //public Rigidbody theRB;
+    public float jumpForce;
+    public CharacterController controller;
+
+    private Vector3 moveDirection;
+    public float gravityScale;
+
+    public float knockBackForce;
+    public float knockBackTime;
+    private float knockBackCounter;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(knockBackCounter <= 0)
+        {
+            float yStore = moveDirection.y;
+            moveDirection = (transform.forward * Input.GetAxis("Vertical")) + (transform.right * Input.GetAxis("Horizontal"));
+            moveDirection = moveDirection.normalized * moveSpeed;
+            moveDirection.y = yStore;
+
+            if(controller.isGrounded)
+            {
+                moveDirection.y = 0f;
+                if(Input.GetButtonDown("Jump"))
+                {
+                    moveDirection.y = jumpForce;
+                }
+            }
+        } else
+        {
+            knockBackCounter -= Time.deltaTime;
+        }
+
+        moveDirection.y = moveDirection.y + (Physics.gravity.y * gravityScale * Time.deltaTime);
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+
+    public void Knockback(Vector3 direction)
+    {
+        knockBackCounter = knockBackTime;
+
+        moveDirection = direction * knockBackForce;
+        moveDirection.y = knockBackForce;
+    }
+}
